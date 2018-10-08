8c2fdcddf471a8a5f7c7af2ccb93a2cdc03673e5
arturadamian <arturdamian@gmail.com>
2018-10-08 21:36:18 +0000

    add some structs

--- 0x01-warm_up/search_algos.h
+++ 0x01-warm_up/search_algos.h
@@ -3,10 +3,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
 
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
 
+typedef struct binary_tree_s binary_tree_t;
 
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t left, size_t right);
 int _binary_search(int *array, size_t left, size_t right, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+int binary_tree_is_full(const binary_tree_t *tree);
+void binary_tree_print(const binary_tree_t *);
 #endif
