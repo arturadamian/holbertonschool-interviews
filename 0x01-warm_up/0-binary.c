292ac61e19b73c7af69c1ff0ab699e2ffd9ecaae
arturadamian <arturdamian@gmail.com>
2018-10-08 20:52:28 +0000

    use recursion now

--- 0x01-warm_up/0-binary.c
+++ 0x01-warm_up/0-binary.c
@@ -11,32 +11,23 @@
  *
  * Return: the node or -1
  **/
-int binary_search(int *array, size_t size, int value)
+int _binary_search(int *array, size_t left, size_t right, int value)
 {
-	unsigned int left = 0, right = size - 1, mid = 0;
+	unsigned int mid = 0;
 
-	if (array)
+	if (right >= left)
 	{
+		mid = left + (right - left) / 2;
 		print_array(array, left, right);
-		while (left < right)
-		{
-			mid = left + (right - left) / 2;
-		if (array[mid] < value)
-		{
-			left = mid + 1;
-			print_array(array, left, right);
-		}
-		else if (array[mid] > value)
-		{
-			right = mid;
-			print_array(array, left, right);
-		}
-		else
+		if (array[mid] == value)
 			return (mid);
-		}
+		if (array[mid] > value)
+			return (_binary_search(array, left, mid - 1, value));
+		return (_binary_search(array, mid + 1, right, value));
 	}
 	return (-1);
 }
+
 /**
  * print_array - prints the array
  * @array: pointed to the first element of the array
@@ -57,4 +48,19 @@ void print_array(int *array, size_t left, size_t right)
 			printf("\n");
 		left++;
 	}
+}
+
+/**
+ * binary_search - main function to find a match
+ * @array: pointer the array
+ * @size: size of the array
+ * @value: value to search
+ *
+ * Return: index or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	if (array)
+		return (_binary_search(array, 0, size - 1, value));
+	return (-1);
 }
