ccb8dfcb217f42475e5599a25cbf7d748cda3936
arturadamian <arturdamian@gmail.com>
2018-10-08 20:52:49 +0000

    header

---
+++ 0x01-warm_up/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t left, size_t right);
+int _binary_search(int *array, size_t left, size_t right, int value);
+#endif
